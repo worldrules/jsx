edd72ab222cbaf6b1aad71781cce6f08513f22b5
worldrules <leonardo.carvalho@acertsis.com.br>
2022-06-28 00:15:12 -0300

    `

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function App() {
+  let message = 'Bye there!'
+
+  if(Math.random() > 0.5) {
+    message = 'Hello There!'
+
+  }
+
+  return <h1>{message}</h1>
+}
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
