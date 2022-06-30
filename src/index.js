988187aff6b20d551a4b2ba11569494c8737cdfb
worldrules <leonardo.carvalho@acertsis.com.br>
2022-06-29 23:06:22 -0300

    faker lib install and comment created

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function App() {
-  const inputType = "number"
-  const minValue = 5
+const App = () => {
 
- return <textarea autoFocus />
+ return (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className='avatar'>
+        <img src="" alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="" className='author'>
+          Sam
+        </a>
+        <div className="metadata">
+          <span className='date'>
+            Today at 6:00PM
+          </span>
+        </div>
+        <div className="text">Nice blog post</div>
+      </div>
+    </div>
+  </div>
+ );
 }
 root.render(
   <React.StrictMode>
