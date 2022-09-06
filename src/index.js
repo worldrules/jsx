95650ea284973d9a1a4cf2b812879889f86175c9
worldrules <leonardo.carvalho@acertsis.com.br>
2022-09-06 15:22:15 -0300

    props added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const App = () => {
 
  return (
   <div className="ui container comments">
-    <CommentDetail/>
+    <CommentDetail author="Sam"/>
+    <CommentDetail author="Alex"/>
+    <CommentDetail author="Jane"/>
   </div>
    
   
