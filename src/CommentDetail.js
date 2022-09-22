3c7297351ce892577daf02528a1eeb7b4d2d405e
worldrules <leonardo.carvalho@acertsis.com.br>
2022-09-22 18:15:48 -0300

    lets rock

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -14,7 +14,7 @@ const CommentDetail = (props) => {
       </a>
       <div className="metadata">
         <span className='date'>
-          Today at 6:00PM
+          {props.timeAgo}
         </span>
       </div>
       <div className="text">Nice blog post</div>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { faker } from '@faker-js/faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CommentDetail from "./CommentDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const App = () => {
-
- return (
-  <div className="ui container comments">
-    <CommentDetail author="Sam"/>
-    <CommentDetail author="Alex"/>
-    <CommentDetail author="Jane"/>
-  </div>
-   
-  
- );
-}
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice Blog Post"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="I like this subject"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I like the writing"
+      />
+    </div>
+  );
+};
 root.render(
   <React.StrictMode>
     <App />
